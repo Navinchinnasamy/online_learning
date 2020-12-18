471f6f7266e076a2e1923fefd4da5eba857192fa
Navin <navinchinnasamy@gmail.com>
2020-12-18 22:12:56 +0530

    Develop branch with build files in root directory.
    Signed-off-by: Navin <navinchinnasamy@gmail.com>

---
+++ src/Components/Team.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+export default class Team extends Component {
+	render(){
+		return (
+			<div className="edu_team_main_wrapper float_left">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-12 col-md-12 col-sm-12">
+							<div className="edu_heading_wrapper float_left">
+								<h3>OUR SPECIAL TEAM</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor, libero non
+									<br />interdum maximus, ligula nulla pretium ligula, a egestas ex nibh.</p>
+							</div>
+						</div>
+						<div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 self_align_center">
+							<div className="edu_team_main_box">
+								<h3><a href="#">Jacklen Fandores</a></h3>
+								<p>Web Designer</p>
+								<ul>
+									<li><a href="#"><i className="fab fa-facebook-f"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-twitter"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-youtube"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-linkedin-in"></i></a>
+									</li>
+								</ul>
+								<div className="edu_team_inner_box">
+									<img src="assets/images/it1.jpg" alt="img" />
+								</div>
+							</div>
+						</div>
+						<div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 self_align_center">
+							<div className="edu_team_main_box">
+								<h3><a href="#">Jacklen Fandores</a></h3>
+								<p>Web Designer</p>
+								<ul>
+									<li><a href="#"><i className="fab fa-facebook-f"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-twitter"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-youtube"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-linkedin-in"></i></a>
+									</li>
+								</ul>
+								<div className="edu_team_inner_box">
+									<img src="assets/images/it2.jpg" alt="img" />
+								</div>
+							</div>
+						</div>
+						<div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 self_align_center">
+							<div className="edu_team_main_box">
+								<h3><a href="#">Jacklen Fandores</a></h3>
+								<p>Web Designer</p>
+								<ul>
+									<li><a href="#"><i className="fab fa-facebook-f"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-twitter"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-youtube"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-linkedin-in"></i></a>
+									</li>
+								</ul>
+								<div className="edu_team_inner_box">
+									<img src="assets/images/it3.jpg" alt="img" />
+								</div>
+							</div>
+						</div>
+						<div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 self_align_center">
+							<div className="edu_team_main_box">
+								<h3><a href="#">Jacklen Fandores</a></h3>
+								<p>Web Designer</p>
+								<ul>
+									<li><a href="#"><i className="fab fa-facebook-f"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-twitter"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-youtube"></i></a>
+									</li>
+									<li><a href="#"><i className="fab fa-linkedin-in"></i></a>
+									</li>
+								</ul>
+								<div className="edu_team_inner_box">
+									<img src="assets/images/it4.jpg" alt="img" />
+								</div>
+							</div>
+						</div>
+						<div className="col-md-12">
+							<div className="edu_bottom_btn_wrapper float_left"> <a href="#">View All</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
