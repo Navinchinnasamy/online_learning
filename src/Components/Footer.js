5d25353e4db4feb37fe701f29f116fa5c6b24caa
Navin <navinchinnasamy@gmail.com>
2020-12-18 20:17:47 +0530

    Home page design with static dummy content
    Signed-off-by: Navin <navinchinnasamy@gmail.com>

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+	render(){
+		return (
+			<div className="edu_footer_bottom float_left">
+				<div className="container">
+					<div className="bottom_footer float_left">
+						<p>&copy;2020-21 Education. All Rights Reserved. <span>Thanks to <a target="_blank" href="https://www.theidioms.com">Idioms</a>.</span></p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
